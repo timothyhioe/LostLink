b1956ceb349d98e5a6a1f3b9c966e7bc275b675c
Nicholas <wilsonsuwirmans@gmail.com>
2025-11-21 20:29:58 +0100

    Feat(frontend): Setup frontend done with react + typescript, no tailwind yet, if needed just say to me/you can install it in the frontend folder and push it. Setup tested with simple Home.tsx

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import './Home.css'
+import testPicture from '../assets/testFolder/test-picture.png'
+import testPicture2 from '../assets/testFolder/test-picture-2.webp'
+
+export default function Home() {
+  return (
+    <div>
+        <p className='home-test-text'>Home.tsx</p>
+        <p className='home-test-text'>image test:</p>
+
+        <img src={testPicture}/>
+                <img src={testPicture2}/>
+
+    </div>
+  )
+}
