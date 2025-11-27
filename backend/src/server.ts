74de39efee4e6a5636493376830a9df67681438e
timothyhioe <timothy.hioe@gmail.com>
2025-11-27 17:19:25 +0000

    add storage service and image upload middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,24 @@
-import http from 'http';
-import { Server as SocketIOServer } from 'socket.io';
+import http from "http";
+import { Server as SocketIOServer } from "socket.io";
 
-import { app } from './app';
-import { connectDatabase } from './config/database';
-import { env } from './config/env';
-import { ensureBucketExists } from './config/minio';
-import { logger } from './utils/logger';
-import { registerChatGateway } from './websocket/chatGateway';
+import { app } from "./app";
+import { connectDatabase } from "./config/database";
+import { env } from "./config/env";
+import { logger } from "./utils/logger";
+import { registerChatGateway } from "./websocket/chatGateway";
+import { ensureBucketExists } from "./config/minio";
 
 async function bootstrap(): Promise<void> {
   await connectDatabase();
   await ensureBucketExists();
+  logger.info("MinIO bucket ready");
 
   const server = http.createServer(app);
   const io = new SocketIOServer(server, {
     cors: {
-      origin: env.CORS_ORIGIN === '*' ? true : env.CORS_ORIGIN.split(','),
-      credentials: true
-    }
+      origin: env.CORS_ORIGIN === "*" ? true : env.CORS_ORIGIN.split(","),
+      credentials: true,
+    },
   });
 
   registerChatGateway(io);
@@ -28,7 +29,6 @@ async function bootstrap(): Promise<void> {
 }
 
 bootstrap().catch((error) => {
-  logger.error('Failed to start server', error);
+  logger.error("Failed to start server", error);
   process.exit(1);
 });
-
